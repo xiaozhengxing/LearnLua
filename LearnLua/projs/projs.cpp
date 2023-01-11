497d6e0c6fb39615bf9740c761ce28ca339e0e0d
xiaozhengxing <1084287435@qq.com>
2023-01-11 23:18:17 +0800

    新建lua源码工程

---
+++ LearnLua/projs/projs.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cassert>
+#include <windows.h>
+
+using namespace  std;
+extern "C"
+{
+#include "LuaSrc/lua.h"
+#include "LuaSrc/lauxlib.h"
+#include "LuaSrc/lualib.h"
+#include "LuaSrc/ltable.h"
+#include "LuaSrc/lstate.h"
+#include "LuaSrc/lobject.h"
+#include "LuaSrc/lapi.h"
+#include "LuaSrc/lauxlib.h"
+#include "LuaSrc/lgc.h"
+}
+
+int main(int argc, char* argv[])
+{
+    lua_State* L = luaL_newstate();
+    assert(L != NULL);    
+
+    luaopen_base(L);
+    luaL_openlibs(L);
+
+    int iRet = luaL_dofile(L, "test.lua");
+    if (iRet)
+    {
+        cout << "load file test.lua error " << iRet << endl;
+        const char* pErrorMsg = lua_tostring(L, -1);
+        cout << pErrorMsg << endl;
+        lua_close(L);
+        
+        return 0;
+    }
+    return 0;
+}
