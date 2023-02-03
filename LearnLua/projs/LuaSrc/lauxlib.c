b9864e8af6d5bdbf99ce06b58c8a5db101e8b87e
xiaozhengxing <1084287435@qq.com>
2023-02-03 18:25:30 +0800

    添加源码注释

--- LearnLua/projs/LuaSrc/lauxlib.c
+++ LearnLua/projs/LuaSrc/lauxlib.c
@@ -930,7 +930,8 @@ LUALIB_API void luaL_openlib (lua_State *L, const char *libname,
 ** set functions from list 'l' into table at top - 'nup'; each
 ** function gets the 'nup' elements at the top as upvalues.
 ** Returns with only the table at the stack.
-*  当前栈{[table][upvalue]...[upvalue][top]}, 栈中含有nup个upvalue
+*  当前栈 [table][upvalue]...[upvalue][top]
+*  nup: 栈中的upvalue个数
 * xzxtodo
 */
 LUALIB_API void luaL_setfuncs (lua_State *L, const luaL_Reg *l, int nup) {
@@ -940,6 +941,7 @@ LUALIB_API void luaL_setfuncs (lua_State *L, const luaL_Reg *l, int nup) {
     for (i = 0; i < nup; i++)  /* copy upvalues to the top */
       lua_pushvalue(L, -nup);
     lua_pushcclosure(L, l->func, nup);  /* closure with those upvalues */
+    //[table][upvalue]...[upvalue][CClosure][top]
     lua_setfield(L, -(nup + 2), l->name);
   }
   lua_pop(L, nup);  /* remove upvalues */
